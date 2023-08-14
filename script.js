04a8fde0e3b3cdf74595bf188cba000c527ac04b
Harshit Borchetia <131516317+HelcurTtTtTtTTtTtT@users.noreply.github.com>
2023-08-14 16:56:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,49 @@ document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 })
+
+
+
+
+// Replace your existing download button event listener code with the following
+
+// Add this code after your existing JavaScript code
+
+document.querySelector("#downloadButton").addEventListener("click", () => {
+    const audioBlob = new Blob([speechToBlob(speech.text)], { type: "audio/wav" });
+    const audioUrl = URL.createObjectURL(audioBlob);
+
+    const a = document.createElement("a");
+    a.href = audioUrl;
+    a.download = "speech_audio.wav";
+    a.style.display = "none";
+    
+    document.body.appendChild(a);
+    a.click();
+    
+    URL.revokeObjectURL(audioUrl);
+});
+
+// Function to convert speech text to Blob
+function speechToBlob(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    const audioChunks = [];
+
+    utterance.onboundary = event => {
+        if (event.name === "word") {
+            audioChunks.push(event.target.voiceData);
+        }
+    };
+
+    const audioRecorder = new MediaRecorder(new MediaStream());
+    audioRecorder.addEventListener("dataavailable", event => {
+        if (event.data.size > 0) {
+            audioChunks.push(event.data);
+        }
+    });
+
+    utterance.voiceData = new Blob(audioChunks, { type: "audio/wav" });
+    window.speechSynthesis.speak(utterance);
+}
+
+// Rest of your existing code
